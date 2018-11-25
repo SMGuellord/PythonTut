769456226a14133791fff8cd72494c157c10410a
SMGuellord <muhiyasumba@gmail.com>
2018-11-25 10:24:06 +0200

    keyword argument

--- moreOnFunctions.py
+++ moreOnFunctions.py
@@ -7,7 +7,21 @@ def get_gender(sex='unknown'):
     print(sex)
 
 
+def dumb_sentence(name='Bucky', action='ate', item='tuna'):
+    print(name, action, item)
+
+
+print("==========================================")
+print("Function with default argument value")
+print("============================================")
 get_gender('m')
 get_gender('f')
 get_gender()
+
+print("==========================================")
+print("Function with keyword argument value")
+print("============================================")
+dumb_sentence("Sally", "eat", "slowly")
+dumb_sentence(item="banana")
+dumb_sentence(name="Sally", action="cooks")
 
