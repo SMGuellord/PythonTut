61257e903f0720bb2dc1924e86a1686fcdefa255
SMGuellord <muhiyasumba@gmail.com>
2018-11-25 10:48:44 +0200

    list and dictionary implementation

---
+++ sets/dictionary.py
@@ -0,0 +1,8 @@
+#dictionary is composed of a key-value pair
+classmates = {'Tony':' cool but smells', 'Emma':' Sits behind me', 'Lucy':' asks too many questions'}
+#print(classmates)
+#print(classmates["Emma"])
+
+for k, v in classmates.items():
+    print(k + v)
+
---
+++ sets/list.py
@@ -0,0 +1,7 @@
+groceries = { 'cereal', 'milk', 'starcrunch', 'beer', 'dark tape', 'lotion', 'beer'}
+print(groceries)
+
+if 'milk' in groceries:
+    print("You already have milk boss!")
+else:
+    print("Oh yea, you need milk!")
